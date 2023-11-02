988deb938f5f6c64925c60d5a79f3fa83c1feb50
KevinK <31565174+kevzzsk@users.noreply.github.com>
2023-11-02 16:02:11 +0800

    feat(eslint): no unused import (#5)
    * chore(ops): add standard-linter
    
    * feat(eslint): enforce file naming + no unused import
    
    * remove pages and layout as it is mostly nextjs convention
    
    * remove enforce name convention
    
    * fix lint
    
    * fix merge conflict
    
    * enforce semicolon
    
    * re add semi
    
    * fix lint check
    
    * set file naming to off
    
    * add new plugins to readme
    
    ---------
    
    Co-authored-by: Nanosync <10258208+Nanosync@users.noreply.github.com>

--- packages/standard-linter/index.js
+++ packages/standard-linter/index.js
@@ -6,11 +6,20 @@ module.exports = {
     "airbnb-typescript/base",
     "prettier",
   ],
-  plugins: ["simple-import-sort", "prettier"],
+  plugins: ["simple-import-sort", "check-file", "unused-imports", "prettier"],
   parser: "@typescript-eslint/parser",
   rules: {
     "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-unused-vars": [
+    "no-console": "error",
+
+    /**
+     * Separates out the `no-unused-vars` rule depending on it being an import statement in the AST and providing
+     * an auto-fix rule to remove the nodes if they are imports.
+     * With this, we can now target test files with `'unused-imports/no-unused-vars': 'off'` for testing DX.
+     */
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
       "error",
       {
         argsIgnorePattern: "^_",
@@ -18,16 +27,35 @@ module.exports = {
         caughtErrorsIgnorePattern: "^_",
       },
     ],
+
+    // import sort
     "sort-imports": "off",
     "import/order": "off",
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
+
+    // To be overridden by each project
+    "check-file/filename-naming-convention": "off",
   },
   overrides: [
     {
-      files: ["**/**.{test,spec,unit}.{js,ts}"],
+      files: ["**/*.{test,spec,unit,i9n,e2e}.{js,ts}"],
       rules: {
-        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-floating-promises": 0,
+        /**
+         * To cater for complex test scenarios, where we need to scope blocks. This allows variables to be reused,
+         * so we don't have to create `const getObject` and `const updatedObject` for each scenario.
+         * We can just use `const object`.
+         */
+        "no-lone-blocks": "off",
+
+        /**
+         * Separates out the `no-unused-vars` rule depending on it being an import statement in the AST and providing
+         * an auto-fix rule to remove the nodes if they are imports.
+         * With this, we can now target test files with `'unused-imports/no-unused-vars': 'off'` for testing DX.
+         */
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": "off",
       },
     },
   ],
