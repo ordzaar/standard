4747593693862283dbc9d007ff620e72c2861dc9
Benedict Pak <10258208+Nanosync@users.noreply.github.com>
2023-11-02 12:13:32 +0800

    fix(linter): change incorrect package name (#10)

--- packages/standard-linter/index.js
+++ packages/standard-linter/index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "airbnb",
+    "airbnb-base",
     "airbnb-typescript/base",
     "prettier",
   ],
